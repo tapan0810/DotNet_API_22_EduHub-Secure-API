123e686b43b4188653fab6f39ebaf11b38b21b0e
tapan0810 <arceus0810@gmail.com>
2026-03-06 09:39:07 +0530

    feat: Implemented JwtHelper for creating Tokens For User Register and Login

---
+++ DotNet_API_22_/Helper/JwtHelper/IJwtHelper.cs
@@ -0,0 +1,10 @@
+﻿using DotNet_API_22_.Entities.Dtos.UserDtos;
+using DotNet_API_22_.Entities.Models;
+
+namespace DotNet_API_22_.Helper.JwtHelper
+{
+    public interface IJwtHelper
+    {
+        public string GenerateToken(User user);
+    }
+}
---
+++ DotNet_API_22_/Helper/JwtHelper/JwtHelper.cs
@@ -0,0 +1,34 @@
+﻿using DotNet_API_22_.Entities.Dtos.UserDtos;
+using DotNet_API_22_.Entities.Models;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DotNet_API_22_.Helper.JwtHelper
+{
+    public class JwtHelper(IConfiguration configuration) : IJwtHelper
+    {
+        public string GenerateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(ClaimTypes.Name,user.UserName),
+                new Claim(ClaimTypes.NameIdentifier,user.Id.ToString())
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(configuration["AppSettings:Token"]!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var tokenDescriptor = new JwtSecurityToken(
+                issuer: configuration.GetValue<string>("AppSettings:Issuer"),
+                audience: configuration.GetValue<string>("AppSettings:Audience"),
+                claims: claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+                );
+            return new JwtSecurityTokenHandler().WriteToken(tokenDescriptor);
+        }
+    }
+}
